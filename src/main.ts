bfa5e85240be4aee8739db1f81f9b5d42b40b96b
DJMarshallSantos <deejaymarshall@msn.com>
2022-09-27 22:35:35 -0400

    refactor swagger

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { setupSwagger } from './shared/swagger/setup-swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  const config = new DocumentBuilder()
-    .setTitle('')
-    .setDescription('')
-    .setVersion('1.0.0')
-    .addTag('Status')
-    .addTag('Auth')
-    .addTag('User')
-    .addTag('Category')
-    .addTag('')
-    .addBearerAuth()
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  setupSwagger(app);
 
   app.enableCors();
 
